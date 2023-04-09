b6dfe1a18b7b7dff06391b418a59f6368da14803
TravTheBav <tzito93@gmail.com>
2023-04-09 14:02:48 -0500

    Web Share api for mobile and select browsers; share to reddit otherwise

--- script.js
+++ script.js
@@ -10,3 +10,30 @@ window.onscroll = function () {
         navBar.classList.remove("nav-colored");
     }
 };
+
+let shareButton = document.getElementById('share-button');
+let shareDialog = document.querySelector('.share-dialog');
+shareButton.addEventListener('click', function() {
+    if (navigator.share) {
+        navigator.share({
+            title: document.title,
+            url: 'https://travthebav.github.io/matzo-soup-recipe/'
+        }).then(() => {
+            console.log('Thanks for sharing!');
+        })
+        .catch(console.error);        
+    } else {
+        shareDialog.classList.add('is-open');
+    }
+});
+
+let exitShareDialogButton = document.querySelector('button.exit');
+exitShareDialogButton.addEventListener('click', function() {
+    shareDialog.classList.remove('is-open');
+});
+
+let redditShareButton = document.getElementById('reddit');
+redditShareButton.addEventListener('click', function () {
+    window.open("https://www.reddit.com/submit?url=https://travthebav.github.io/matzo-soup-recipe/&title=Classic%20Matzo%20Ball%20Soup%20Recipe", "_blank");
+});
+
